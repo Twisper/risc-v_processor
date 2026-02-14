fc48780ab53960e6c4b70c00d75b5e1fe8779a60
Twisper <83642379+Twisper@users.noreply.github.com>
2026-02-15 00:31:46 +0300

    Feat: Added 32-bit operations support in shifter

--- alu/shifter.sv
+++ alu/shifter.sv
@@ -6,11 +6,12 @@
 * @date Feb 2026
 *
 * @description
-* This file contains Barrel Shifter, which is able to do 5 types of shifting (Logical Left and Right, Arithmetic right, Rotate Left and Right from Zbb Extension).
+* This file contains Barrel Shifter, which is able to do 5 types of shifting (Logical Left and Right, Arithmetic right, Rotate Left and Right from Zbb Extension) with 32 and 64 bit operands.
 * Key advantages:
 * - Universal and fast shifter for 5 types of shifts.
 * - Uses only 575 Multiplexors (checked with yosys).
-* - Is built for doing right shifts, but for left shifts reverses operand and result. 
+* - Is built for doing right shifts, but for left shifts reverses operand and result.
+* - Supports 32-bit and 64-bit shifts at the same time. 
 *
 * Interface:
 * - Inputs: One N-bit operand, shift distance, type of shift and direction
@@ -24,16 +25,45 @@
 module shifter
     #(parameter WIDTH = 64, parameter DEPTH = $clog2(WIDTH))
     (
-    input   logic [WIDTH-1:0] a, //Operand to shift. 
-    input   logic [DEPTH-1:0] shift, //Shift distance. 
-    input   logic [1:0]       shift_type, //Type of shifting. 00 - logical, 01 - arithmetical, 10 - rotation. 
+    input   logic [WIDTH-1:0] operand, //Operand to shift. 
+    input   logic [DEPTH-1:0] shamt, //Shift distance. 
+    input   logic [1:0]       shift_op, //Type of shifting. 00 - logical, 01 - arithmetical, 10 - rotation. 
     input   logic             shift_direction, //Direction of shift. 0 - left, 1 - right. 
-    output  logic [WIDTH-1:0] y //Result
+    input   logic             is_32_bit_mode, //Puts shifter into 32-bit operand mode. 
+    output  logic [WIDTH-1:0] result
 );
 
     logic [DEPTH:0][WIDTH-1:0] temp; //Packed array to store results of each stage of shift. 
-    logic [WIDTH-1:0] concat_1, concat_2; //Temporary variables for left shifts. 
+    logic [WIDTH-1:0]          concat_1, concat_2; //Temporary variables for left shifts. 
+    logic [WIDTH-1:0]          a, y; //Variables for temporary input/output.
+    logic [DEPTH-1:0]          shift;
+    logic [1:0]                shift_type;
     genvar i;
+    localparam int LOWWIDTH = WIDTH/2;
+
+    /*
+    * This always_comb block is nessesary for 32-bit operations (For example, SLLW, SRLW, SRAW, RORW, ROLW, etc.). 
+    * Depending on shift operation, it extends high 32-bit operand to 64-bit, on order to make 32-bit and 64-bit operations similar. 
+    */
+
+    always_comb begin
+        shift_type = shift_op;
+        if (is_32_bit_mode) begin //Checking whether is 32-bit mode. 
+            case (shift_op) //If yes, checking shift operation for higher bit extenion. 
+                2'b00: a = {{LOWWIDTH{1'b0}}, operand[LOWWIDTH-1:0]}; //If it is logical shift, higher bits become zeros. 
+                2'b01: a = {{LOWWIDTH{operand[LOWWIDTH-1]}}, operand[LOWWIDTH-1:0]}; //If it is arithmetic shift, extending higher bits with signed bit. 
+                2'b10: begin
+                    a = {operand[LOWWIDTH-1:0], operand[LOWWIDTH-1:0]}; //It it is ROR/ROL shift, copying lower 32 bits into higher bits. 
+                    shift_type = shift_direction ? 2'b00 : 2'b10;
+                end 
+                default: a = {{LOWWIDTH{1'b0}}, operand[LOWWIDTH-1:0]}; //Default case - logical shift. 
+            endcase
+            shift = {1'b0, shamt[DEPTH-2:0]};
+        end else begin
+            a = operand; //If it is 64-bit mode, doing nothing. 
+            shift = shamt;
+        end
+    end
 
     reverse first_reverse (a, concat_1); //If there is left shift, an operand is being reversed.  
 
@@ -62,7 +92,9 @@ module shifter
 
     reverse second_reverse (temp[DEPTH], concat_2); //If there is left shift, the result is being reversed.  
 
-    assign y = shift_direction ? temp[DEPTH] : concat_2;
+    assign y = shift_direction ? temp[DEPTH] : concat_2; //Preparing final result for extension if it is 32-bit mode. 
+
+    assign result = is_32_bit_mode ? {{LOWWIDTH{y[LOWWIDTH-1]}}, y[LOWWIDTH-1:0]} : y; //Extending final result with signed bit for 64-bit register. 
 
 endmodule
 
